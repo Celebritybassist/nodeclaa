6aff5c3d4f4711b5b44ab55d27b40bedfb0c71d2
aiyedunmiracle956888@gmail.com <aiyedunmiracle956888@gmail.com>
2026-05-20 15:44:46 +0100

    assignment added

--- app.js
+++ app.js
@@ -5,3 +5,21 @@ function findminimumvalue(m, n) {
     console.log(findminimumvalue(-3, 2)); // Output: -3
     console.log(findminimumvalue(0, 0)); // Output: 0
     }
+    // Write a javascript code to reverse your name
+    let name = "Aiyedun Miracle";
+
+    let reversedName = name.split("").reverse().join("");
+
+    console.log(reversedName);
+
+    // write a javascript code to change the value of an array
+    let names = ["Samuel", "Silas", "John"];
+
+    // Change "Samuel" to "Jude"
+    names[1] = "Jude";
+
+    console.log(names);
+
+    // write a javascript code to find the factor of a number using the concept of recursion
+    
+    

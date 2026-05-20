7847fad64f40b4cf539f25a512a0cb7eebdc51eb
aiyedunmiracle956888@gmail.com <aiyedunmiracle956888@gmail.com>
2026-05-20 15:01:51 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function findminimumvalue(m, n) {
+    return m < n ? m : n;
+
+    console.log(findminimumvalue(5, 10)); // Output: 5
+    console.log(findminimumvalue(-3, 2)); // Output: -3
+    }
